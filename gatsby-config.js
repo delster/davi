f6ca0877e2aaf6c114fc29bbe41b1b2c9e689e58
David Elster <davidandtheweb@gmail.com>
2019-05-26 20:48:42 -0400

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Davi`,
+    description: `David's Divi-alternate Components using React, MDX, and Gatsby.`,
+    author: `@delster`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-mdx`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -24,11 +25,10 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    // Progressive Web App + Offline functionality
+    `gatsby-plugin-offline`,
   ],
 }
